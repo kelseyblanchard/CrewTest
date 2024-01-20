444465ea9d6688e4c2e95a6a9fed5873f399dcb1
evanbrooks0629 <60624108+evanbrooks0629@users.noreply.github.com>
2024-01-20 15:46:51 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [input, setInput] = React.useState("");
 
   const submitData = () => {
+    setData("Loading...");
     fetch('http://127.0.0.1:5000/api', {
         method: "POST", // *GET, POST, PUT, DELETE, etc.
         // mode: "no-cors", // no-cors, *cors, same-origin
@@ -22,13 +23,14 @@ function App() {
         .then(response => {
           return response.text();
         })
-        .then(response => setData(response));
+        .then(response => setData(response))
+        .catch(err => setData("There was an error."));
   }
 
   return (
     <div className="App">
       <p></p>
-      <textarea type="text" rows={5} value={input} onChange={e => setInput(e.target.value)} />
+      <textarea type="text" rows={12} value={input} onChange={e => setInput(e.target.value)} style={{width: '600px'}} />
       <p></p>
       <button onClick={submitData}>Submit</button>
       <p>{data}</p>
